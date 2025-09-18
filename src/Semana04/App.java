729a1ae62132638a5f4b39a74e96ce9ea9b77a97
Keiner-Josue <kjbarbosac@ufpso.edu.co>
2025-09-17 20:03:22 -0500

    semana 04

---
+++ src/Semana04/App.java
@@ -0,0 +1,8 @@
+package Semana04;
+
+public class App {
+    public static void main(String[] args) {
+        Calculator cal = new Calculator();
+        System.out.println(cal.Sumar(5, 8));
+    }
+}
---
+++ src/Semana04/Calculator.java
@@ -0,0 +1,10 @@
+package Semana04;
+
+public class Calculator {
+    public int Sumar (int numA , int numB){
+        return numA + numB;
+    }
+    public double Sumar (double numA , double numB){
+        return numA + numB;
+    }
+}
